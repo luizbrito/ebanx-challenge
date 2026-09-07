f1bc302801304e604c4ba2e514d863477b46330f
Luiz Brito da Rosa <luizbritodarosa@gmail.com>
2026-09-07 17:15:15 -0300

    first commit

---
+++ src/main/java/com/luizbrito/ebanx_challenge/EbanxChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.luizbrito.ebanx_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbanxChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbanxChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizbrito/ebanx_challenge/EbanxChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizbrito.ebanx_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbanxChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
